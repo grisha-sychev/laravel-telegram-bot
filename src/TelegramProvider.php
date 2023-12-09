41f9637a304039f9a8b9d93c5904f171b0f42ca0
Gregory Sychev <grisha_sychev@internet.ru>
2023-12-09 19:10:31 +0300

    Initial commit

---
+++ src/TelegramProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Reijo\Telebot\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class TelegramProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../app' => app_path(),
+            __DIR__ . '/../config' => config_path(),
+            __DIR__ . '/../database' => database_path(),
+            __DIR__ . '/../routes' => base_path('routes'),
+        ], 'reijo-telebot');
+    }
+}
