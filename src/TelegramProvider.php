234dff9034361d3d27f0981e8712b1900dced20a
Grisha Sychev <100209515+grisha-sychev@users.noreply.github.com>
2023-12-09 21:33:58 +0300

    Initial commit

--- src/TelegramProvider.php
+++ src/TelegramProvider.php
@@ -20,8 +20,5 @@ class TelegramProvider extends ServiceProvider
             __DIR__ . '/../database' => database_path(),
             __DIR__ . '/../routes' => base_path('routes'),
         ], 'reijo-telebot');
-
-        Route::middleware('web')->withoutMiddleware(['web', 'App\Http\Middleware\VerifyCsrfToken'])
-            ->group(base_path('routes/telegram.php'));
     }
 }
