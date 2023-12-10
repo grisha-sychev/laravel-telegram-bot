a35f524c6d0d3a4079d886fb7f2b632c3b0db9a7
Grisha Sychev <100209515+grisha-sychev@users.noreply.github.com>
2023-12-10 15:21:57 +0300

    Update RouteTelegramProvider.php

--- app/Providers/RouteTelegramProvider.php
+++ app/Providers/RouteTelegramProvider.php
@@ -26,6 +26,13 @@ class RouteTelegramProvider extends ServiceProvider
      */
     public function boot()
     {
+        $this->publishes([
+            __DIR__ . '/../app' => app_path(),
+            __DIR__ . '/../config' => config_path(),
+            __DIR__ . '/../database' => database_path(),
+            __DIR__ . '/../routes' => base_path('routes'),
+        ]);
+        
         $this->routes(function () {
             Route::middleware('web')->withoutMiddleware(['web', 'App\Http\Middleware\VerifyCsrfToken'])
                 ->group(base_path('routes/telegram.php'));
