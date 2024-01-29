ab28a3bfc53c2eb9e23cbf7877f9b93676c67846
Gregory Sychev <grisha_sychev@internet.ru>
2024-01-29 03:02:19 +0300

    string

--- src/Data/MessageQuery.php
+++ src/Data/MessageQuery.php
@@ -29,28 +29,28 @@ class MessageQuery
 
     public function __construct(array $data)
     {
-        $this->data = $data ?? null;
-        $this->updateId = $data['update_id'] ?? null;
-        $this->messageId = $data['message']['message_id'] ?? null;
-        $this->fromId = $data['message']['from']['id'] ?? null;
-        $this->fromIsBot = $data['message']['from']['is_bot'] ?? null;
-        $this->fromFirstName = $data['message']['from']['first_name'] ?? null;
-        $this->fromUsername = $data['message']['from']['username'] ?? null;
-        $this->chatId = $data['message']['chat']['id'] ?? null;
-        $this->chatFirstName = $data['message']['chat']['first_name'] ?? null;
-        $this->chatUsername = $data['message']['chat']['username'] ?? null;
-        $this->chatType = $data['message']['chat']['type'] ?? null;
-        $this->messageDate = $data['message']['date'] ?? null;
-        $this->messageText = $data['message']['text'] ?? null;
-        $this->video = $data['message']['video'] ?? null;
-        $this->audio = $data['message']['audio'] ?? null;
-        $this->photo = $data['message']['photo'] ?? null;
-        $this->sticker = $data['message']['sticker'] ?? null;
-        $this->contact = $data['message']['contact'] ?? null;
-        $this->location = $data['message']['location'] ?? null;
-        $this->voice = $data['message']['voice'] ?? null;
-        $this->document = $data['message']['document'] ?? null;
-        $this->date = $data['message']['date'] ?? null;
+        $this->data = $data ?? '';
+        $this->updateId = $data['update_id'] ?? '';
+        $this->messageId = $data['message']['message_id'] ?? '';
+        $this->fromId = $data['message']['from']['id'] ?? '';
+        $this->fromIsBot = $data['message']['from']['is_bot'] ?? '';
+        $this->fromFirstName = $data['message']['from']['first_name'] ?? '';
+        $this->fromUsername = $data['message']['from']['username'] ?? '';
+        $this->chatId = $data['message']['chat']['id'] ?? '';
+        $this->chatFirstName = $data['message']['chat']['first_name'] ?? '';
+        $this->chatUsername = $data['message']['chat']['username'] ?? '';
+        $this->chatType = $data['message']['chat']['type'] ?? '';
+        $this->messageDate = $data['message']['date'] ?? '';
+        $this->messageText = $data['message']['text'] ?? '';
+        $this->video = $data['message']['video'] ?? '';
+        $this->audio = $data['message']['audio'] ?? '';
+        $this->photo = $data['message']['photo'] ?? '';
+        $this->sticker = $data['message']['sticker'] ?? '';
+        $this->contact = $data['message']['contact'] ?? '';
+        $this->location = $data['message']['location'] ?? '';
+        $this->voice = $data['message']['voice'] ?? '';
+        $this->document = $data['message']['document'] ?? '';
+        $this->date = $data['message']['date'] ?? '';
     }
     public function getAll()
     {
