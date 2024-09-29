923f0331ea92d8970cb965d438d436188fc1379b
Grisha Sychev <100209515+grisha-sychev@users.noreply.github.com>
2024-09-29 18:29:09 +0300

    3.0.0

--- src/Providers/TelegramBootstrapServiceProvider.php
+++ src/Providers/TelegramBootstrapServiceProvider.php
@@ -14,13 +14,23 @@ class TelegramBootstrapServiceProvider extends ServiceProvider
      *
      * @return void
      */
+    
     public function boot()
     {
-        $this->publishes([
+        $pathsToPublish = [
             __DIR__ . '/../../app' => app_path(),
             __DIR__ . '/../../config' => config_path(),
             __DIR__ . '/../../database' => database_path(),
             __DIR__ . '/../../routes' => base_path('routes'),
-        ], "laravel-assets");
+        ];
+
+        $pathsToPublish = array_filter($pathsToPublish, function ($source) {
+            return file_exists($source);
+        }, ARRAY_FILTER_USE_KEY);
+
+        if (!empty($pathsToPublish)) {
+            $this->publishes($pathsToPublish, "laravel-assets");
+        }
     }
 }
+
